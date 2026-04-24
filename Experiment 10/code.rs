82f96c6c32cd7958cdb2440ca01f56e72d564ab7
Vanshit Kumar <136151242+vanshitkumar@users.noreply.github.com>
2026-04-24 21:01:52 +0530

    Experiment 10

--- Experiment 10/code.rs
+++ Experiment 10/code.rs
@@ -1,23 +1,54 @@
-fn count_smaller(nums: Vec<i32>) -> Vec<i32> {
-    let mut bit = vec![0; 20002];
-    let mut res = vec![0; nums.len()];
-    
-    for i in (0..nums.len()).rev() {
-        let val = (nums[i] + 10001) as usize;
-        
+struct SegTree {
+    tree: Vec<i32>,
+    n: usize,
+}
+
+impl SegTree {
+    fn new(n: usize) -> Self {
+        SegTree {
+            tree: vec![0; 2 * n],
+            n,
+        }
+    }
+
+    fn update(&mut self, mut pos: usize, val: i32) {
+        pos += self.n;
+        while pos > 0 {
+            self.tree[pos] += val;
+            pos /= 2;
+        }
+    }
+
+    fn query(&self, mut l: usize, mut r: usize) -> i32 {
+        l += self.n;
+        r += self.n;
         let mut sum = 0;
-        let mut q = val - 1;
-        while q > 0 {
-            sum += bit[q];
-            q -= q & q.wrapping_neg();
-        }
-        res[i] = sum;
         
-        let mut u = val;
-        while u < bit.len() {
-            bit[u] += 1;
-            u += u & u.wrapping_neg();
+        while l < r {
+            if l % 2 == 1 {
+                sum += self.tree[l];
+                l += 1;
+            }
+            if r % 2 == 1 {
+                r -= 1;
+                sum += self.tree[r];
+            }
+            l /= 2;
+            r /= 2;
         }
+        
+        sum
+    }
+}
+
+fn count_smaller(nums: Vec<i32>) -> Vec<i32> {
+    let mut seg_tree = SegTree::new(20005);
+    let mut res = vec![0; nums.len()];
+
+    for i in (0..nums.len()).rev() {
+        let val = (nums[i] + 10000) as usize;
+        res[i] = seg_tree.query(0, val);
+        seg_tree.update(val, 1);
     }
     
     res
